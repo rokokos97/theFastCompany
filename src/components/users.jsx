2685b3fdfcfc53c8b82c01bc29166bac97653f56
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-17 18:01:05 +0100

    create conventional rendering badge with phrase

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,22 +5,29 @@ const Users=()=>{
     const [users,setUsers]=useState(api.users.fetchAll())
     console.log(users);
     const renderPhrase=()=>{
-        return <span>Тут будет фраза</span>
+        const renderBadgeClass=()=>{
+            return users.length>0?"primary":"danger"
+        }
+        const phrase = users.length<1?"No one":(users.length>1?`${users.length}`:"Only one")
+        return <span className={`badge bg-${renderBadgeClass()} m-2`}>{`${phrase} people hang out with you tonight`}</span>
     }
-    return <>
-        {/*{renderPhrase()}*/}
-        <table className="table">
-            <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Qualities</th>
-                <th scope="col">Profession</th>
-                <th scope="col">Meets</th>
-                <th scope="col">Rate</th>
-                <th scope="col"></th>
-            </tr>
-            </thead>
-            <tbody>
+    const handelDelete=(id)=>{
+        setUsers(users.filter((filteredUser)=>filteredUser._id!==id))
+    }
+    const createTable=()=>{
+        if(users.length>0){
+            return <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Qualities</th>
+                    <th scope="col">Profession</th>
+                    <th scope="col">Meets</th>
+                    <th scope="col">Rate</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
                 {users.map((userObject)=>
                     <tr key={userObject._id}>
                         <td>{userObject.name}</td>
@@ -28,11 +35,16 @@ const Users=()=>{
                         <td>{userObject.profession.name}</td>
                         <td>{userObject.completedMeetings}</td>
                         <td>{userObject.rate}</td>
-                        <td><button className="btn bg-danger">Delete</button></td>
+                        <td><button className="btn bg-danger" onClick={()=>handelDelete(userObject._id)}>Delete</button></td>
                     </tr>
                 )}
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        }
+    }
+    return <>
+        {renderPhrase()}
+        {createTable()}
     </>
 }
 
