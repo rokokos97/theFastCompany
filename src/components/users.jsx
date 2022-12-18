91510c119d66980089de0b7593f3dc48c316f3bb
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-18 11:55:41 +0100

    add table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -5,7 +5,26 @@ const Users=()=>{
     const [users,setUsers]=useState(api.users.fetchAll());
     console.log(users);
     return <>
-
+        <table className="table">
+            <thead>
+            <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col">Кнопка</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            </tbody>
+        </table>
     </>
 }
 
