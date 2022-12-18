fb8a0d31fbbf9a60386eb86f2a7a8e7416b15618
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-18 16:47:34 +0100

    add renderPhrase()

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,9 +3,26 @@ import api from "../api"
 
 const Users=()=>{
     const [users,setUsers]=useState(api.users.fetchAll());
-    console.log(users);
+    const renderPhrase=(number)=>{
+        const lastOne = Number(number.toString().slice(-1))
+        if (number > 4 && number < 15) return "человек тусанет";
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+        if (lastOne === 1) return "человек тусанет";
+        return "человек тусанет";
+    }
+    const handelDelete=(userId)=>
+        setUsers(users.filter((user)=>user._id!==userId))
     return <>
-        <table className="table">
+        <h2>
+            <span
+                className={"m-1 badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+            >
+                    {users.length > 0
+                        ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                        : "Никто с тобой не тусанет"}
+                </span>
+        </h2>
+        {users.length>0&&<table className="table">
             <thead>
             <tr>
                 <th scope="col">Имя</th>
@@ -17,14 +34,27 @@ const Users=()=>{
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
+            {users.map((user)=>(
+                <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{user.qualities.map((item)=>
+                        <span key={item.id} className={"badge m-1 bg-"+item.color}>
+                            {item.name}
+                        </span>)}
+                    </td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}</td>
+                    <td>
+                        <button
+                            className={"btn btn-danger"}
+                            onClick={()=>{handelDelete(user._id)}}>удалить</button>
+                    </td>
+                </tr>
+            ))}
             </tbody>
         </table>
+        }
     </>
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Users from "./components/users";
