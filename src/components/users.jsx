8cfcce0ba7fff6bdeac875a7f2201f286f51e79e
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-18 11:46:56 +0100

    add api

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 import api from "../api"
 
 const Users=()=>{
-    console.log(api.users.fetchAll());
-    return <h1>Users</h1>
+    const [users,setUsers]=useState(api.users.fetchAll());
+    console.log(users);
+    return <>
+
+    </>
 }
 
 export default Users
