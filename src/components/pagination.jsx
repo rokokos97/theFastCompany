8cf1d8de9fe7d042c869176666de5a72e5c0a600
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-20 18:47:43 +0100

    lesson 3 week 3 install _lodash complete pages in pagination.jsx

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import _ from "lodash";
 
 const Pagination = (props) => {
- return null
+  const { itemCont, pageSize } = props;
+  const pageCount = Math.ceil(itemCont / pageSize);
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+  return (
+    <>
+      <nav>
+        <ul className="pagination">
+          {pages.map((page) => (
+            <li key={"page_" + page} className="page-itam">
+              <a href="#" className="page-link">
+                {page}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Pagination;
