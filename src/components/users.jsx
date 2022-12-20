57acacdb54d47495a810fb08f9fd549d4908c185
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-20 17:58:34 +0100

    lesson 3 week 3 Add component Pagination to users.jsx

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 import User from "./user";
+import Pagination from "./pagination";
 const Users = ({ users, ...rest }) => {
-    return (
-        <>
-            {users.length > 0 && (
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
-                        <th scope="col">Провфессия</th>
-                        <th scope="col">Встретился, раз</th>
-                        <th scope="col">Оценка</th>
-                        <th scope="col">Избранное</th>
-                        <th />
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {users.map((user) => (
-                        <User key={user._id} {...rest} {...user} />
-                    ))}
-                    </tbody>
-                </table>
-            )}
-        </>
-    );
+  const count = users.length;
+  const pageSize = 4;
+  const handelPageChange = (pageIndex) => {
+    console.log("pageIndex", pageIndex);
+  };
+
+  return (
+    <>
+      {count > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Имя</th>
+              <th scope="col">Качества</th>
+              <th scope="col">Провфессия</th>
+              <th scope="col">Встретился, раз</th>
+              <th scope="col">Оценка</th>
+              <th scope="col">Избранное</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <User key={user._id} {...rest} {...user} />
+            ))}
+          </tbody>
+        </table>
+      )}
+      <Pagination
+        itemCont={count}
+        pageSize={pageSize}
+        onPageChange={handelPageChange}
+      />
+    </>
+  );
 };
 
 export default Users;
