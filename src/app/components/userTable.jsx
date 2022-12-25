d3ef1aefa39f28fdb10b963f99c8e02cd374668b
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-26 00:08:29 +0100

    add component: <Bookmark/>

--- src/app/components/userTable.jsx
+++ src/app/components/userTable.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
+import Bookmark from "./bookmark";
 
-const UserTable = ({ users, onSort, selectedSort }) => {
+const UserTable = ({ users, onSort, selectedSort, onToggleBookMark }) => {
     const columns = {
         name: { path: "name", name: "Name" },
         qualities: { name: "Qualities" },
         profession: { path: "profession.name", name: "Profession" },
         completedMeetings: { path: "completedMeetings", name: "Meets" },
         rate: { path: "rate", name: "Rate" },
-        bookmark: { path: "bookmark", name: "Bookmark", component: "Bookmark" },
+        bookmark: {
+            path: "bookmark",
+            name: "Bookmark",
+            component: <Bookmark
+                status={bookmark}
+                onClick={() => onToggleBookMark(_id)}
+            /> },
         delete: { component: "Delete" }
     };
     return (
@@ -24,6 +31,7 @@ const UserTable = ({ users, onSort, selectedSort }) => {
 UserTable.propTypes = {
     selectedSort: PropTypes.object,
     users: PropTypes.array.isRequired,
-    onSort: PropTypes.func
+    onSort: PropTypes.func.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
 };
 export default UserTable;
