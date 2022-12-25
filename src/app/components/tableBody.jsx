15384eb25071a4806f0d92971944932d1fbac822
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-12-25 12:27:35 +0100

    get profession.name dynamic with _lodash (_.get())

--- src/app/components/tableBody.jsx
+++ src/app/components/tableBody.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import _ from "lodash"
 const TableBody = ({ data, columns }) => {
     return <>
         { data.map((item) =>
             <tr key={item._id}>
                 {Object.keys(columns).map((column) =>
-                    <td key={item[columns[column].path]} >{item[columns[column].path]}</td>
+                    <td key={column} >{_.get(item, columns[column].path)}</td>
                 )}
             </tr>
         )}
--- src/app/components/userTable.jsx
+++ src/app/components/userTable.jsx
@@ -18,11 +18,6 @@ const UserTable = ({ users, onSort, selectedSort, ...rest }) => {
         <table className="table">
             <TableHeader selectedSort={selectedSort} onSort={onSort} columns={columns}/>
             <TableBody data={users} columns={columns}/>
-            {/*<tbody>*/}
-            {/*    {users.map((user) => (*/}
-            {/*        <User key={user._id} {...rest} {...user} />*/}
-            {/*    ))}*/}
-            {/*</tbody>*/}
         </table>
     );
 };
