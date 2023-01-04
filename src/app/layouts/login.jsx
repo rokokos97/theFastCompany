2f6fc94bd386126e99fd631463d7b162426d5cb6
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-04 12:07:33 +0100

    Create form in login.jsx {" "} add space between <label> and <input>

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -5,14 +5,14 @@ const Login = () => {
         <>
             <form action="" className={"m-2"}>
                 <div className={"m-2"}>
-                    <label htmlFor={"email"}>Email</label>
+                    <label htmlFor={"email"}>Email</label>{" "}
                     <input
                         type={"text"}
                         id={"email"}
                         placeholder={"Enter your email"}/>
                 </div>
                 <div className={"m-2"}>
-                    <label htmlFor={"password"}>Password</label>
+                    <label htmlFor={"password"}>Password</label>{" "}
                     <input type={"password"} id={"password"} placeholder={"Enter your password"}/>
                 </div>
             </form>
