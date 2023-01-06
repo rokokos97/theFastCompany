7572184adbdca59376825f912f38096cbe242d30
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-06 16:47:38 +0100

    login.jsx => add method toggleFormType

--- src/app/components/ ui/registerForm.jsx
+++ src/app/components/ ui/registerForm.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
-
 const RegisterForm = () => {
- return <>
-     <div className={"container mt-5"}>
-         <div className="row">
-             <div className={"col-md-6 offset-md-3 shadow p-3"}>
-             <div/>
-             <div/>
-             <div/></>;
+    return <></>;
 };
 
 export default RegisterForm;
--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../components/ ui/loginForm";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import RegisterForm from "../components/ ui/registerForm";
 const Login = () => {
     const { type } = useParams();
-    const [ formType, setFormType ] = useState(type === "register" ? type : "login");
+    const [formType, setFormType] = useState(type === "register" ? type : "login");
+    const toggleFormType = () =>{
+
+    };
     return (
         <div className={"container mt-5"}>
             <div className="row">
                 <div className={"col-md-6 offset-md-3 shadow p-3"}>
-                    <LoginForm/>
+                    {formType === "register"
+                        ? <>
+                            <RegisterForm/>
+                            <p>Already have an account?
+                                <a href="">Sign in</a>
+                            </p>
+                        </>
+                        : <>
+                            <LoginForm/>
+                            <p>Dont have an account?
+                                <a href="">Sign up</a>
+                            </p>
+                        </>}
                 </div>
             </div>
         </div>
