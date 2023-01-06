8f7fbd30c9f27f2ab487bf9dcf19d2d3f59797bc
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-06 10:55:12 +0100

    add style to userPage.jsx

--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -15,14 +15,43 @@ const UserPage = ({ userId }) => {
     };
     if (user) {
         return (
-            <div>
-                <h1> {user.name}</h1>
-                <h2>Профессия: {user.profession.name}</h2>
-                <QualitiesList qualities={user.qualities} />
-                <p>completedMeetings: {user.completedMeetings}</p>
-                <h2>Rate: {user.rate}</h2>
-                <button onClick={handleClick}> Все Пользователи</button>
-            </div>
+            <>
+                <div className="container">
+                    <div className="row">
+                        <div className="card text-center col-md-6 offset-md-3">
+                            {/* <div className="card-header"> */}
+                            {/*    <ul className="nav nav-pills card-header-pills"> */}
+                            {/*        <li className="nav-item"> */}
+                            {/*            <a className="nav-link active" href="#">Active</a> */}
+                            {/*        </li> */}
+                            {/*        <li className="nav-item"> */}
+                            {/*            <a className="nav-link" href="#">Link</a> */}
+                            {/*        </li> */}
+                            {/*        <li className="nav-item"> */}
+                            {/*            <a className="nav-link disabled">Disabled</a> */}
+                            {/*        </li> */}
+                            {/*    </ul> */}
+                            {/* </div> */}
+                            <div className="card-body">
+                                <h5 className="card-title">{user.name}</h5>
+                                <p className="card-text">Profession: {user.profession.name}</p>
+                                <p className="card-text"><QualitiesList qualities={user.qualities} /></p>
+                                <p className="card-text">completedMeetings: {user.completedMeetings}</p>
+                                <p className="card-text">Rate: {user.rate}</p>
+                                <button className={"btn btn-primary w-100 wx-auto"} onClick={handleClick}> All users</button>
+                            </div>
+                        </div>
+                        {/* <div> */}
+                        {/*    <h1> {user.name}</h1> */}
+                        {/*    <h2>Profession: {user.profession.name}</h2> */}
+                        {/*    <QualitiesList qualities={user.qualities} /> */}
+                        {/*    <p>completedMeetings: {user.completedMeetings}</p> */}
+                        {/*    <h2>Rate: {user.rate}</h2> */}
+                        {/*    <button onClick={handleClick}> Все Пользователи</button> */}
+                        {/* </div> */}
+                    </div>
+                </div>
+            </>
         );
     } else {
         return <h1>Loading</h1>;
