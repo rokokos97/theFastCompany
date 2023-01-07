fb0c080e37e46250a323f829b842196f12c546a9
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-07 17:39:27 +0100

    create radioField.jsx

---
+++ src/app/components/common/form/radioField.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const RadioField = ({ options, value, name, onChange, label }) => {
+    return (
+        <>
+            <div className="mb-4">
+                <div>
+                    <label className={"form-label"}>{label}</label>
+                </div>
+                {options.map((option) => (
+                    <div
+                        key={option.name + "_" + option.value}
+                        className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name={name}
+                            checked={option.value === value}
+                            id={option.name + "_" + option.value}
+                            value={option.value}
+                            onChange={onChange}
+                        />
+                        <label
+                            className="form-check-label"
+                            htmlFor={option.name + "_" + option.value}
+                        >
+                            {option.name}
+                        </label>
+                    </div>))}
+            </div>
+        </>
+    );
+};
+RadioField.propTypes = {
+    label: PropTypes.string,
+    options: PropTypes.array,
+    value: PropTypes.string,
+    name: PropTypes.string,
+    onChange: PropTypes.func
+};
+export default RadioField;
