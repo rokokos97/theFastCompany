b7385ac574f2bcc067769802f04f5f20f1e0c914
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-14 12:08:52 +0100

    editUserPage.jsx transfer user data from api to editUserPage.jsx

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -9,8 +9,8 @@ import { useHistory, useParams } from "react-router-dom";
 
 const EditUserPage = () => {
     const { userId } = useParams();
-    const [qualities, setQualities] = useState({});
-    const [professions, setProfessions] = useState([]);
+    const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState({
         name: "",
         email: "",
@@ -21,8 +21,20 @@ const EditUserPage = () => {
         qualities: [{}]
     });
     const [errors, setErrors] = useState({});
-
+    const [qualities, setQualities] = useState({});
+    const [professions, setProfessions] = useState([]);
+    const transformData = (data) => {
+        return data.map((qual) => ({ label: qual.name, value: qual._id }));
+    };
     useEffect(() => {
+        api.users.getById(userId).then(({ profession, qualities, ...data }) => setData((prevState) => (
+            {
+                ...prevState,
+                ...data,
+                profession: profession._id,
+                qualities: transformData(qualities)
+            }
+        )));
         api.professions.fetchAll().then((data) => {
             const professionList =
                 Object.keys(data).map((professionName) => ({
@@ -31,7 +43,15 @@ const EditUserPage = () => {
                 }));
             setProfessions(professionList);
         });
-        api.qualities.fetchAll().then((data) => setQualities(data));
+        api.qualities.fetchAll().then((data) => {
+            const qualitiesList =
+            Object.keys(data).map((qualitiesName) => ({
+                label: data[qualitiesName].name,
+                value: data[qualitiesName]._id,
+                color: data[qualitiesName].color
+            }));
+            setQualities(qualitiesList);
+        });
     }, []);
     const handelChange = (target) => {
         console.log("target", target.name);
@@ -76,73 +96,75 @@ const EditUserPage = () => {
     const isValid = Object.keys(errors).length === 0;
     return (<div className="container">
         <div className="row">
-            <div className="card col-md-6 offset-md-3">
-                <div className="card-body">
-                    <form onSubmit={handelSubmit} className={"card-body"}>
-                        <TextFiled
-                            label={"Name"}
-                            name={"name"}
-                            value={data.name}
-                            onChange={handelChange}
-                            error={errors.name}
-                        />
-                        <TextFiled
-                            label={"Email"}
-                            name={"email"}
-                            value={data.email}
-                            onChange={handelChange}
-                            error={errors.email}
-                        />
-                        <TextFiled
-                            label={"Rate"}
-                            name={"rate"}
-                            value={data.email}
-                            onChange={handelChange}
-                            error={errors.rate}
-                        />
-                        <TextFiled
-                            label={"Completed Meetings"}
-                            name={"completedMeetings"}
-                            value={data.completedMeetings}
-                            onChange={handelChange}
-                            error={errors.completedMeetings}
-                        />
-                        <RadioField
-                            label={"Choose your sex"}
-                            options={[{ name: "Male", value: "male" },
-                                { name: "Female", value: "female" },
-                                { name: "Other", value: "other" }]}
-                            value={data.sex}
-                            name={"sex"}
-                            onChange={handelChange}
-                        />
-                        <SelectedField
-                            label={"Choose your profession"}
-                            name={"profession"}
-                            defaultOption={"Choose..."}
-                            options={professions}
-                            onChange={handelChange}
-                            value={data.profession}
-                            error={errors.profession}
-                        />
-                        <MultiSelectField
-                            options={qualities}
-                            onChange={handelChange}
-                            defaultOption={data.qualities}
-                            name={"qualities"}
-                            label={"Choose your qualities"}
-                            error={errors.qualities}
-                        />
-                        <button
-                            type={"submit"}
-                            className={"btn btn-success w-100 mx-auto"}
-                            disabled={!isValid}
-                        >
-                                    Save info
-                        </button>
-                    </form>
+            {(!isLoading && Object.keys(professions).length > 0 && Object.keys(qualities).length > 0)
+                ? <div className="card col-md-6 offset-md-3">
+                    <div className="card-body">
+                        <form onSubmit={handelSubmit} className={"card-body"}>
+                            <TextFiled
+                                label={"Name"}
+                                name={"name"}
+                                value={data.name}
+                                onChange={handelChange}
+                                error={errors.name}
+                            />
+                            <TextFiled
+                                label={"Email"}
+                                name={"email"}
+                                value={data.email}
+                                onChange={handelChange}
+                                error={errors.email}
+                            />
+                            <TextFiled
+                                label={"Rate"}
+                                name={"rate"}
+                                value={data.email}
+                                onChange={handelChange}
+                                error={errors.rate}
+                            />
+                            <TextFiled
+                                label={"Completed Meetings"}
+                                name={"completedMeetings"}
+                                value={data.completedMeetings}
+                                onChange={handelChange}
+                                error={errors.completedMeetings}
+                            />
+                            <RadioField
+                                label={"Choose your sex"}
+                                options={[{ name: "Male", value: "male" },
+                                    { name: "Female", value: "female" },
+                                    { name: "Other", value: "other" }]}
+                                value={data.sex}
+                                name={"sex"}
+                                onChange={handelChange}
+                            />
+                            <SelectedField
+                                label={"Choose your profession"}
+                                name={"profession"}
+                                defaultOption={"Choose..."}
+                                options={professions}
+                                onChange={handelChange}
+                                value={data.profession}
+                                error={errors.profession}
+                            />
+                            <MultiSelectField
+                                options={qualities}
+                                onChange={handelChange}
+                                defaultValue={data.qualities}
+                                name={"qualities"}
+                                label={"Choose your qualities"}
+                                error={errors.qualities}
+                            />
+                            <button
+                                type={"submit"}
+                                className={"btn btn-success w-100 mx-auto"}
+                                disabled={!isValid}
+                            >
+                                        Save info
+                            </button>
+                        </form>
+                    </div>
                 </div>
-            </div>
+                : "Loading form..."}
         </div>
     </div>
     );
