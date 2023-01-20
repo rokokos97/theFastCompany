8746b95e2880c1c03aa0531b3d68a7fa85edfa52
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-20 23:23:16 +0100

    create qualitiesCard.jsx.jsx

---
+++ src/app/components/ui/qualitiesCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import QualitiesList from "./qualities";
+
+const QualitiesCard = ({ qualities }) => {
+    return <>
+        <div className="card mb-3">
+            <div className="card-body d-flex flex-column justify-content-center text-center">
+                <h5 className="card-title">
+                    <span>Qualities</span>
+                </h5>
+                <p className="card-text">
+                    <QualitiesList qualities={qualities}/>
+                </p>
+            </div>
+        </div>
+    </>;
+};
+QualitiesCard.propTypes = {
+    qualities: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
+};
+export default QualitiesCard;
