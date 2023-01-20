311e619a3b381c0ae89fd95aee6dfe6f644324f6
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-21 00:07:26 +0100

    add commentsList.jsx add comment.jsx

--- src/app/components/ui/comments.jsx
+++ src/app/components/ui/comments.jsx
@@ -1,14 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import api from "../../api";
+import _ from "lodash"
 
 const Comments = () => {
     const { userId } = useParams();
     const [comments, setComments] = useState([]);
     useEffect(() => {
-
+        api.comments.fetchCommentsForUser(userId).then((data) => setComments(data));
     }, []);
-    return <>Comments</>;
+    const sortedComments = _.orderBy(comments, ["created_at"], ["desc"]);
+    return <>
+        <div className="card mb-2">
+            {" "}
+            <div className="card-body ">
+                <AddCommentForm onSubmit={handleSubmit} />
+            </div>
+        </div>
+        {sortedComments.length > 0 && (
+            <div className="card mb-3">
+                <div className="card-body ">
+                    <h2>Comments</h2>
+                    <hr />
+                    <CommentsList
+                        comments={sortedComments}
+                        onRemove={handleRemoveComment}
+                    />
+                </div>
+            </div>
+        )}
+    </>
 };
 
 export default Comments;
