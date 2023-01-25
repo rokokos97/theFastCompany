bcaab00f117b25485ec214739fa0bbd3afaf1887
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-25 22:57:30 +0100

    create "hooks" folder add useUsers.jsx => creatr useUser
    use UserProvider in [layout].users.jsx

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -3,14 +3,22 @@ import { useParams } from "react-router-dom";
 import UserPage from "../components/page/userPage/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import EditUserPage from "../components/page/editUserPage";
+import UserProvider from "../hooks/useUsers";
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
-    return <>{userId
-        ? (edit
-            ? <EditUserPage/>
-            : <UserPage userId={userId} />)
-        : <UsersListPage />}</>;
+    return (
+        <>
+            <UserProvider>
+                {userId
+                    ? (edit
+                        ? <EditUserPage/>
+                        : <UserPage userId={userId} />)
+                    : <UsersListPage />
+                }
+            </UserProvider>
+        </>
+    );
 };
 
 export default Users;
