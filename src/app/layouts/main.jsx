41da047ca776537ec211b481a137c4098b0138fc
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-27 14:21:23 +0100

    create new HOOK useMockData

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import useMockData from "../utils/mockData";
 
 const Main = () => {
-    const { initialize } = useMockData();
+    const { initialize, status, progress, error } = useMockData();
     const handleClick = () => {
         console.log("clicked");
         initialize();
@@ -11,6 +11,11 @@ const Main = () => {
         <div className="container mt-5">
             <h1>Main page</h1>
             <h3>Initial data in FireBase</h3>
+            <ul>
+                <li>Status: {status}</li>
+                <li>Progress: {progress}%</li>
+                {error && <li>Error: {error}</li>}
+            </ul>
             <button className={"btn btn-primary"} onClick={handleClick}>
                 {" "}
                 Initial
