8e7eb1d3d021a4b3f14f493eb89b102e18949400
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-17 11:48:35 +0100

    fix bug in loginForm.jsx

--- src/app/components/ui/loginForm.jsx
+++ src/app/components/ui/loginForm.jsx
@@ -34,7 +34,6 @@ const LoginForm = () => {
         if (!isValid) return;
         try {
             await logIn(data);
-            console.log(history.location.state.from);
             history.push(
                 history.location.state
                     ? history.location.state.from.pathname
