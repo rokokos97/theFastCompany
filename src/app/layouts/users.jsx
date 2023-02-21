91635528418013077a82a86a19a32e6f793e2a16
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-21 23:05:51 +0100

    users.jsx => add <Redirect to={`/users/${currentUser._id}/edit`}/> (16-19)

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Redirect } from "react-router-dom";
 import UserPage from "../components/page/userPage/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import EditUserPage from "../components/page/editUserPage";
 import UserProvider from "../hooks/useUsers";
+import { useAuth } from "../hooks/useAuth";
 const Users = () => {
+    const { currentUser } = useAuth();
     const params = useParams();
     const { userId, edit } = params;
     return (
@@ -12,7 +14,9 @@ const Users = () => {
             <UserProvider>
                 {userId
                     ? (edit
-                        ? <EditUserPage/>
+                        ? (userId === currentUser._id)
+                            ? <EditUserPage/>
+                            : <Redirect to={`/users/${currentUser._id}/edit`}/>
                         : <UserPage userId={userId} />)
                     : <UsersListPage />
                 }
