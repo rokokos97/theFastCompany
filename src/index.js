6b63e82a8b881eabd73ee58e08bd1c2dcf5fe92a
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-24 18:41:44 +0100

    index.js => <BrowserRouter> change to <Router history={history}>

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import App from "./app/App";
 import { createStore } from "./app/store/createStore";
 import { Provider } from "react-redux";
+import history from "./app/utils/history";
 
 const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <Router history={history}>
         <Provider store={store}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
         </Provider>
-    </BrowserRouter>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
