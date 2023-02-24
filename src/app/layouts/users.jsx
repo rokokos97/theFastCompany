f2e7da2da22d4fb84dfbec31ed0e9a2e4398f108
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-02-24 23:13:55 +0100

    create HOC => usersLoader.jsx

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,34 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useParams, Redirect } from "react-router-dom";
 import UserPage from "../components/page/userPage/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import EditUserPage from "../components/page/editUserPage";
 import UserProvider from "../hooks/useUsers";
-import { useAuth } from "../hooks/useAuth";
-import { useDispatch, useSelector } from "react-redux";
-import { getDataStatus, loadUsersList } from "../store/users";
+import { useSelector } from "react-redux";
+import { getCurrentUser } from "../store/users";
+import UsersLoader from "../components/ui/HOC/usersLoader";
 const Users = () => {
-    const { currentUser } = useAuth();
+    const currentUserId = useSelector(getCurrentUser());
     const params = useParams();
     const { userId, edit } = params;
-    const dataStatus = useSelector(getDataStatus());
-    const dispatch = useDispatch;
-    useEffect(() => {
-        if (!dataStatus) dispatch(loadUsersList());
-    }, []);
-    if (!dataStatus) return "Loading...";
     return (
         <>
-            <UserProvider>
-                {userId
-                    ? (edit
-                        ? (userId === currentUser._id)
-                            ? <EditUserPage/>
-                            : <Redirect to={`/users/${currentUser._id}/edit`}/>
-                        : <UserPage userId={userId} />)
-                    : <UsersListPage />
-                }
-            </UserProvider>
+            <UsersLoader>
+                <UserProvider>
+                    {userId
+                        ? (edit
+                            ? (userId === currentUserId)
+                                ? <EditUserPage/>
+                                : <Redirect to={`/users/${currentUserId}/edit`}/>
+                            : <UserPage userId={userId} />)
+                        : <UsersListPage />
+                    }
+                </UserProvider>
+            </UsersLoader>
         </>
     );
 };
