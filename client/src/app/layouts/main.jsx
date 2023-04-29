886e9d52404a40386dec5c2e9cbd7902b5f92d2c
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-04-29 12:39:16 +0200

    translate app

--- client/src/app/layouts/main.jsx
+++ client/src/app/layouts/main.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import getRandomInt from "../utils/getRandomInt";
+import config from "../config.json"
 const Main = () => {
     return (
         <div className="container mt-5">
             <h1 className="text-center"> The Fast Company </h1>
             <div className="d-flex justify-content-center flex-wrap">
-                <img src={`https://robohash.org/set_set5/bgset_bg2/3.122418${getRandomInt(1,20)}?size=270x270`} alt="Some face"/>
-                <img src={`https://robohash.org/set_set5/bgset_bg2/3.1224${getRandomInt(1,20)}18?size=270x270`} alt="Some face"/>
-                <img src={`https://robohash.org/set_set5/bgset_bg2/3.1224${getRandomInt(1,20)}18${getRandomInt(1,20)}?size=270x270`} alt="Some face"/>
-                <img src={`https://robohash.org/set_set5/bgset_bg2/3.12${getRandomInt(1,20)}2418${getRandomInt(1,20)}?size=270x270`} alt="Some face"/>
+                <img src={`${config.apiEndpoint}/upload/${getRandomInt(1,4)}.png`} alt="Some face"/>
+                <img src={`${config.apiEndpoint}/upload/${getRandomInt(5,8)}.png`} alt="Some face"/>
+                <img src={`${config.apiEndpoint}/upload/${getRandomInt(9,12)}.png`} alt="Some face"/>
+                <img src={`${config.apiEndpoint}/upload/${getRandomInt(13,15)}.png`} alt="Some face"/>
+                {/*<img src={`https://robohash.org/set_set5/bgset_bg2/3.1224${getRandomInt(1,20)}18?size=270x270`} alt="Some face"/>*/}
+                {/*<img src={`https://robohash.org/set_set5/bgset_bg2/3.1224${getRandomInt(1,20)}18${getRandomInt(1,20)}?size=270x270`} alt="Some face"/>*/}
+                {/*<img src={`https://robohash.org/set_set5/bgset_bg2/3.12${getRandomInt(1,20)}2418${getRandomInt(1,20)}?size=270x270`} alt="Some face"/>*/}
             </div>
         </div>
     );
--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
-
+app.use('/api/upload', express.static('uploads'));
 app.use('/api', routes)
 
 
